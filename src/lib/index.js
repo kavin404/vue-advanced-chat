9d2c60abf1815997c1a48be59b7b4830450b3c51
Kavindu Chinthana <kavinduchinthana@gmail.com>
2021-06-29 20:17:24 +0530

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -8,4 +8,21 @@ Object.defineProperty(ChatWindow, 'install', {
 	}
 })
 
+
+// Create module definition for Vue.use()
+const plugin = {
+	install,
+};
+
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+	GlobalVue.use(plugin);
+}
+
 export default ChatWindow
